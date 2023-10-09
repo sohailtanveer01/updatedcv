e0eb7d817054c22031b606a1c620bc6c9703ea85
md sohail <sohail@mds-MacBook-Pro.local>
2023-10-09 13:38:27 -0500

    removed _ENV from gitignore

---
+++ client/public/__ENV.js
@@ -0,0 +1,1 @@
+window.__ENV = {"PUBLIC_URL":"http://localhost:3000","PUBLIC_GOOGLE_CLIENT_ID":"644053299262-sahbprql0fu77cr32i3ns9ikupk6p8nf.apps.googleusercontent.com","PUBLIC_FLAG_DISABLE_SIGNUPS":"false","REACT_ENV_PREFIX":"PUBLIC"};
