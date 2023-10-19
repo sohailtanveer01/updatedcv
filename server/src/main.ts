6856bbda349af3f7c096005cd41a0eb2b4b00551
md sohail <sohail@mds-MacBook-Pro.local>
2023-10-19 15:31:02 -0500

    changed appurl in main

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,11 @@ const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
-  const appUrl = configService.get('app.url');
+  // const appUrl = configService.get('app.url');
+
+  const appUrl = process.env.PUBLIC_URL
+  console.log("this is appurl",appUrl)
+  
 
   // Middleware
   app.enableCors({ origin: [appUrl], credentials: true });
