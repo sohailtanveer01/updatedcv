eb76a584ecb36534a666de7b4f83af9f4fe36962
md sohail <sohail@mds-MacBook-Pro.local>
2023-10-19 13:02:28 -0500

    pointing to absolute url

--- client/services/auth.ts
+++ client/services/auth.ts
@@ -115,9 +115,9 @@ export const updateSubscriberfun = async (updateProfileParams: UpdateSubscriberP
 
 
 export const loginclerk = async(newUserObject:object)=>{
-  // const apiUrl:string = process.env.PUBLIC_SERVER_URL!;
+  const apiUrl:string = process.env.PUBLIC_SERVER_URL!;
 
-  const loginUrl = '/auth/clerklogin';
+  const loginUrl = `${apiUrl}/auth/clerklogin`;
   const {
     data: { user, accessToken },
   }= await axios.post(loginUrl,newUserObject)
